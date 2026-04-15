76d57bc58cb7d9167d2387893fa4a0ea9eb28de0
n2m0contact-create <n2m0.contact@gmail.com>
2026-04-15 21:02:59 +0900

    Add files via upload

--- character一覧/chara.js
+++ character一覧/chara.js
@@ -1,10 +1,10 @@
 
 window.charaList = [
     {
-        name: "アリス",
-        kana: "ありす",
+        name: "アザリア",
+        kana: "あざりあ",
         type: ["human", "female", "mother_cell", "g.other"],
-        tags: ["人", "女性", "コピー", "その他"],
+        tags: ["人", "女性", "幹", "その他"],
         desc: "仮キャラクター。一般通過アンドロイド。寿司よりもたこ焼きの入ってない焼きが好き。",
         img: "character_img/alice.png",
         link: ""
@@ -23,16 +23,121 @@ window.charaList = [
 
     /* 魔族 */
     //R1
+    {
+        name: "モナ・ウーヌス",
+        kana: "もな・うーぬす",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1での1体目のボス。",
+        img: "character_img/",
+        link: ""
+    },
+
+
+    {
+        name: "シルフィ",
+        kana: "しるふぃ",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1での2体目のボス。",
+        img: "character_img/",
+        link: ""
+    },
+
+
+    {
+        name: "ステレス",
+        kana: "すてれす",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1で壁の管理を行っている一般魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+    {
+        name: "ヴィド・バイボラー",
+        kana: "びど・ばいぼらー",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1で無職をしている一般魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+    {
+        name: "メシア",
+        kana: "めしあ",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1で占い業をしている一般魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+
+    {
+        name: "グラビノ",
+        kana: "ぐらびの",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1でよく通報される一般魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+
+    {
+        name: "ヒトガタ",
+        kana: "ひとがた",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1で3体目のボス。",
+        img: "character_img/",
+        link: ""
+    },
+
+    {
+        name: "クロウ",
+        kana: "くろう",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1で魔王城内で働く一般魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+    {
+        name: "ロロス・エス",
+        kana: "ろろす・えす",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1に跋扈する歩く卑猥魔族。",
+        img: "character_img/",
+        link: ""
+    },
+
+
+    {
+        name: "Dogma",
+        kana: "どぐま",
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
+        desc: "R1 4体目のボス。",
+        img: "character_img/",
+        link: ""
+    },
+
 
     //R2
     {
         name: "Mekanizm",
         kana: "めかにずむ",
-        type: ["unhuman", "female", "mother_cell", "g.other"],
-        tags: ["魔族", "無性", "コピー", "その他"],
+        type: ["unhuman", "unknown", "daughter_cell", "otukai"],
+        tags: ["魔族", "無性", "枝", "おつかい"],
         desc: "R2での4体目のボス。",
         img: "character_img/mekanizm.png",
-        link: ""
+        link: "Character/Mecanizm/Mecanizm.html"
     },
     //R3
 
@@ -70,8 +175,35 @@ window.charaList = [
     /*--------------------------------------------------
         * mother_cell　1 (他)キャラクター一覧
     ----------------------------------------------------*/
+    {
+        name: "酒粕",
+        kana: "さけかす",
+        type: ["human", "unknown", "mother_cell", "g.other", "dead"],
+        tags: ["人", "無性", "幹", "その他", "死亡"],
+        desc: "アル中の煤人。信用がない。",
+        img: "character_img/",
+        link: ""
+    },
 
+    {
+        name: "©＃",
+        kana: "コピーライト＿シャープ",
+        type: ["human", "unknown", "mother_cell", "g.other"],
+        tags: ["人", "無性", "幹", "その他"],
+        desc: "モニター頭のサイボーグ。",
+        img: "character_img/",
+        link: ""
+    },
 
+    {
+        name: "死教者",
+        kana: "しきょうしゃ",
+        type: ["human", "unknown", "mother_cell", "g.other","alive"],
+        tags: ["人", "無性", "幹", "その他","生存"],
+            desc: "推定鳥系のキメラ。死に心酔している。",
+            img: "character_img/",
+            link: ""
+    },
 
     /*--------------------------------------------------
         * その他　キャラクター一覧
